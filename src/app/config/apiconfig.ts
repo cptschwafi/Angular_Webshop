184ed5c2fcf7de4d6e39154cc58bf0fc1c2c8645
Andreas <schwarzfischer.a@gmail.com>
2021-06-11 13:57:00 +0200

    Added ScrollToTop Button edit

--- src/app/config/apiconfig.ts
+++ src/app/config/apiconfig.ts
@@ -1,6 +1,6 @@
 import { environment } from '../../environments/environment';
 
-export const baseUrl = environment.production ? 'https://ProductionServer' : 'http://localhost:3000';
+export const baseUrl = environment.production ? 'https://schwafisplace.sytes.net' : 'http://localhost:3000';
 // export const baseUrl = environment.production ? 'https://schwafisplace.sytes.net/' : 'https://schwafisplace.sytes.net/';
 export const productsUrl = baseUrl + '/products';
 export const cartUrl = baseUrl + '/carts';
